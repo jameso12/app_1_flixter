f4ff845f23359adf3adde5d1ee91856140b3ee8e
jameso12 <jamesorodz@gmail.com>
2022-03-04 21:09:39 -0400

    before the gifs

--- app/src/main/java/com/example/flixter/DetailActivity.kt
+++ app/src/main/java/com/example/flixter/DetailActivity.kt
@@ -2,26 +2,84 @@ package com.example.flixter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings.Secure.getString
 import android.util.Log
 import android.widget.RatingBar
 import android.widget.TextView
+import com.codepath.asynchttpclient.AsyncHttpClient
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
+import com.google.android.youtube.player.YouTubeBaseActivity
+import com.google.android.youtube.player.YouTubeInitializationResult
+import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
+import okhttp3.Headers
 
+private var YOUTUBE_API_KEY = BuildConfig.YOUTUBEAPIKEY
 private const val TAG = "DetailActivity"
-class DetailActivity : AppCompatActivity() {
+private const val TRAILERS_URL = "https://api.themoviedb.org/3/movie/%d/videos?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed"
+class DetailActivity : YouTubeBaseActivity() {
     private lateinit var tvTitle: TextView
     private lateinit var tvOverview: TextView
     private lateinit var rbRatingBar: RatingBar
+    private lateinit var ytPlayerView: YouTubePlayerView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         tvTitle = findViewById(R.id.tvTitle)
         tvOverview = findViewById(R.id.tvOverview)
         rbRatingBar = findViewById(R.id.rbMovieRatingAVG)
+        ytPlayerView = findViewById(R.id.player)
         // We cast to Movie(default is nullable).
         val movie = intent.getParcelableExtra<Movie>(MOVIE_EXTRA) as Movie
         Log.i(TAG, "Movie: ${movie.title}")
         tvTitle.text = movie.title
         tvOverview.text = movie.overview
         rbRatingBar.rating = movie.rating.toFloat() // rating bar expects a float so we cast
+        val client = AsyncHttpClient()
+        client.get(TRAILERS_URL.format(movie.movieID), object : JsonHttpResponseHandler(){
+            override fun onFailure(
+                statusCode: Int,
+                headers: Headers?,
+                response: String?,
+                throwable: Throwable?
+            ) {
+                Log.i(TAG, "onFailure: $statusCode")
+            }
+
+            override fun onSuccess(statusCode: Int, headers: Headers?, json: JSON) {
+                Log.i(TAG, "onSuccess")
+                val results = json.jsonObject.getJSONArray("results")
+                if(results.length()==0){
+                    Log.w(TAG, "No movie trailers found")
+                    return
+                }
+                val movieTrailerJson = results.getJSONObject(0)
+                val youtubeKey = movieTrailerJson.getString("key")
+                initialzeYoutube(youtubeKey)
+
+            }
+
+        })
+    }
+
+    private fun initialzeYoutube(youtubeKey: String) {
+        ytPlayerView.initialize(YOUTUBE_API_KEY,object: YouTubePlayer.OnInitializedListener{
+            override fun onInitializationSuccess(
+                provider: YouTubePlayer.Provider?,
+                player: YouTubePlayer?,
+                p2: Boolean
+            ) {
+                Log.i(TAG, "Initialized successfully")
+                player?.cueVideo(youtubeKey)
+            }
+
+            override fun onInitializationFailure(
+                p0: YouTubePlayer.Provider?,
+                p1: YouTubeInitializationResult?
+            ) {
+                Log.i(TAG, "Initialization failed.")
+            }
+
+        })
     }
 }
